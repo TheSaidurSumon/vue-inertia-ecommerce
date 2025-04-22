ce871e9e00cf6d0481412667011a3e7f194c1cb3
Saidur Rahman Bhuiyan <55763087+TheSaidurSumon@users.noreply.github.com>
2025-04-22 19:06:47 +0600

    Add files via upload

---
+++ EditorApp/storage/framework/views/a5cb5ec539379dc0caa6df475aa0eca2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title><?php echo e($title ?? 'EditorApp - Dashboard'); ?></title>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.js', 'resources/css/app.css']); ?>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
+  </head>
+  <body>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
+  </body>
+</html><?php /**PATH C:\Users\Lenovo\Desktop\laravel ecommerce\b5_ecom\EditorApp\resources\views/app.blade.php ENDPATH**/ ?>
